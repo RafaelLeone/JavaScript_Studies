af5fb973e5a2fcedd461f1e6ab46709bd4125c84
RafaelLeone <rinaciodacosta@gmail.com>
2022-08-11 10:32:01 -0300

    02-03

---
+++ js02-03.js
@@ -0,0 +1,19 @@
+/**
+* Faça um programa que faça a média de todos os nros de uma lista
+* 
+* Tarefa:
+* - use node para rodar este script js
+*   $ node js02-02.js
+*   👉 Ímpar
+*/
+
+var check = [10, 9.5, 8, 490]
+var total = 0
+for (var i of check) {
+    total += i
+}
+
+var media = total / check.length
+var resultado = '👉 ' + media
+
+console.log(resultado)
